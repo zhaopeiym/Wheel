fa29f424c5e35937a2c7bd6764c822b74148dca0
benny <zhaopeiym@163.com>
2017-08-25 17:45:39 +0800

    优化hiSwitch

--- 轮子/开关/hiSwitch/jquery.hiSwitch.js
+++ 轮子/开关/hiSwitch/jquery.hiSwitch.js
@@ -7,22 +7,37 @@
         if (data === true) //执行关闭操作
         {
             $(this).data('key', false).attr('title', '已关闭');
-            $(this).children('span').html('开启').removeClass("switchClose").addClass("switchOpen");
+            $(this).children('span').html('已关闭').removeClass("switchClose").addClass("switchOpen");
         }
         else {
             $(this).data('key', true).attr('title', '已开启');
-            $(this).children('span').html('关闭').removeClass("switchOpen").addClass("switchClose");
+            $(this).children('span').html('已开启').removeClass("switchOpen").addClass("switchClose");
         }
         //触发自定义事件
         $(this).trigger("hiSwitchChanged");
     })
 
     $.fn.extend({
-        //初始化（如果有setSwitch设置值，就不需要初始化了）
-        initHiSetSwitch: function () {            
-            $(this).each(function (i, e) {
-                $(e).setHiSwitch($(e).data("key"));
-            });
+        //初始化（如果有setSswitch设置值，就不需要初始化了）
+        initHiSetSwitch: function () {
+            //如果选择的是Switch容器
+            if ($(this).find('.hiSwitch').length > 0) {
+                $(this).find('.hiSwitch').each(function (i, e) {
+                    if ($(e).find("span").length <= 0)
+                        $(e).append("<span></span>");
+                    $(e).setHiSwitch($(e).data("key"));
+                });
+            }
+            //如果选择的是Switch控件
+            else if ($(this).find('.hiSwitch').length <= 0 && $(this).hasClass('hiSwitch')) {
+                $(this).each(function (i, e) {
+                    if ($(e).hasClass("hiSwitch")) {
+                        if ($(e).find("span").length <= 0)
+                            $(e).append("<span></span>");
+                        $(e).setHiSwitch($(e).data("key"));
+                    }
+                });
+            }
         },
         //设置 开关值
         setHiSwitch: function (state) {
@@ -31,7 +46,7 @@
         },
         //获取 开关值
         getHiSwitch: function (state) {
-            return $(this).data("key");            
+            return $(this).data("key");
         }
     });
 })(jQuery, window);
